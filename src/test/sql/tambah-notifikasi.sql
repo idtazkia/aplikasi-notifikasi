f728425722f6bec555b0a57e97975731e2eb51fb
Haffizh <haffizh@tazkia.ac.id>
2018-03-20 13:53:52 +0700

    sql insert notifikasi (test)

--- src/test/sql/tambah-notifikasi.sql
+++ src/test/sql/tambah-notifikasi.sql
@@ -1,35 +1,39 @@
+-- insert into template_sms
+INSERT INTO template_sms (id, id_sender, description, template_content)
+VALUES ('sms-pmb-kartu-jpa', 'pmb', 'Template SMS Kartu Jpa',
+        '');
+-- insert into template_email
+INSERT INTO template_email (id, id_sender, description, subject, file_location)
+VALUES ('email-pmb-kartu-jpa', 'pmb', 'Template Registrasi PMB Kartu jpa', 'Pengisian Data a.n {{nama}} & Perintah Pengumpulan berkas', 'jpa.html');
+
+
 -- insert ke tabel notification_configuration
-INSERT INTO notification_configuration (id, id_sender, id_template_email, id_template_sms, configuration_name, description)
-VALUES ('pmb-kartuujian', 'pmb', 'email-pmb-registrasi-kartu-ujian', 'sms-pmb-registrasi-kartu-ujian', 'KartuUjian', 'Notifikasi Kartu Ujian');
+INSERT INTO notification_configuration (id, id_sender, id_template_email, id_template_sms, configuration_name, description)VALUES
+ ('pmb-kartu-jpa', 'pmb', 'email-pmb-kartu-jpa', 'sms-pmb-kartu-jpa', 'KartuJpa', 'Notifikasi Perintah Pengumpulan Berkas Jpa');
 
 -- insert into tabel notification_configuration_variable
 INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
-VALUES ('pmb-kartuujian-nomor', 'pmb-kartuujian', 'nomor', 'Nomor Registrasi', TRUE);
+VALUES ('pmb-kartu-jpa-nomor', 'pmb-kartu-jpa', 'nomor', 'Nomor Registrasi', TRUE);
 
 INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
-VALUES ('pmb-kartuujian-nama', 'pmb-kartuujian', 'nama', 'Nama', TRUE);
-INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
-VALUES ('pmb-kartuujian-id', 'pmb-kartuujian', 'id', 'Id Pendaftar', TRUE);
+VALUES ('pmb-kartu-jpa-nama', 'pmb-kartu-jpa', 'nama', 'Nama', TRUE);
 
 INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
-VALUES ('pmb-kartuujian-namakontak2', 'pmb-kartuujian', 'namaKontak2', 'Nama Contact Person 2', TRUE);
+VALUES ('pmb-kartu-jpa-email', 'pmb-kartu-jpa', 'email', 'Email', TRUE);
 
 INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
-VALUES ('pmb-kartuujian-nomorkontak2', 'pmb-kartuujian', 'nomorKontak2', 'Nomor HP Contact Person 2', TRUE);
+VALUES ('pmb-kartu-jpa-noHp', 'pmb-kartu-jpa', 'noHp', 'No Hp', TRUE);
 
 INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
-VALUES ('pmb-kartuujian-namakontak3', 'pmb-kartuujian', 'namaKontak3', 'Nama Contact Person 3', TRUE);
+VALUES ('pmb-kartu-jpa-namakontak1', 'pmb-kartu-jpa', 'namaKontak1', 'Nama Contact Person 1', TRUE);
 
 INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
-VALUES ('pmb-kartuujian-nomorkontak3', 'pmb-kartuujian', 'nomorKontak3', 'Nomor HP Contact Person 3', TRUE);
+VALUES ('pmb-kartu-jpa-nomorkontak1', 'pmb-kartu-jpa', 'nomorKontak1', 'Nomor HP Contact Person 1', TRUE);
 
--- insert into template_sms
-INSERT INTO template_sms (id, id_sender, description, template_content)
-VALUES ('sms-pmb-registrasi-kartu-ujian', 'pmb', 'Template SMS Kartu Ujian',
-        'Terima kasih telah melengkapi data a.n {{nama}}. Untuk tahap selanjutnya silahkan DOWNLOAD kartu ujian di email Anda. Info {{namaKontak1}}-{{nomorKontak1}}');
+INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
+VALUES ('pmb-kartu-jpa-namakontak3', 'pmb-kartu-jpa', 'namaKontak3', 'Nama Contact Person 3', TRUE);
 
--- insert into template_email
-INSERT INTO template_email (id, id_sender, description, subject, file_location)
-VALUES ('email-pmb-registrasi-kartu-ujian', 'pmb', 'Template Registrasi PMB Kartu Ujian', 'Pengisian Data a.n {{nama}} & Kartu Ujian', 'kartuUjian.html');
+INSERT INTO notification_configuration_variable (id, id_notification_configuration, variable_name, description, required)
+VALUES ('pmb-kartu-jpa-nomorkontak3', 'pmb-kartu-jpa', 'nomorKontak3', 'Nomor HP Contact Person 3', TRUE);
 
 
